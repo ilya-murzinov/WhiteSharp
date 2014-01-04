13d4986f824a7294f2b9cbeea9df754405e0023c
murz42 <murz42@gmail.com>
2014-01-04 10:28:17 -0800

    Added sample test.

---
+++ src/SampleTests/CalculatorTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+using WhiteSharp;
+using WhiteSharp.UnitTests;
+
+namespace SampleTests
+{
+    [TestFixture]
+    public class CalculatorTest : TestBaseNUnit
+    {
+        [TestCase("130", "0")]
+        [TestCase("131", "1")]
+        [TestCase("132", "2")]
+        [TestCase("133", "3")]
+        [TestCase("134", "4")]
+        [TestCase("135", "5")]
+        [TestCase("136", "6")]
+        [TestCase("137", "7")]
+        [TestCase("138", "8")]
+        [TestCase("139", "9")]
+        public void NumberButtonsTest(string buttonId, string result)
+        {
+            UIControl button = MainWindow.FindControl(By.AutomationId(buttonId)).Click();
+            Assert.True(Display.Name.Equals(result));
+        }
+        
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("4")]
+        [TestCase("5")]
+        [TestCase("6")]
+        [TestCase("7")]
+        [TestCase("8")]
+        [TestCase("9")]
+        [TestCase("0")]
+        public void Add(string str)
+        {
+            UIControl plus = MainWindow.FindControl(By.AutomationId("93"));
+            UIControl equals = MainWindow.FindControl(By.AutomationId("121"));
+            Random r = new Random();
+            int i = (int)r.Next(1000);
+            int j = (int)r.Next(1000);
+            Display.Click().Send(i.ToString());
+            plus.Click();
+            Display.Click().Send(j.ToString());
+            equals.Click();
+            Assert.AreEqual(Display.Name, (i + j).ToString());
+        }
+
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("4")]
+        [TestCase("5")]
+        [TestCase("6")]
+        [TestCase("7")]
+        [TestCase("8")]
+        [TestCase("9")]
+        [TestCase("0")]
+        public void Multiply(string str)
+        {
+            UIControl mult = MainWindow.FindControl(By.AutomationId("92"));
+            UIControl equals = MainWindow.FindControl(By.AutomationId("121"));
+            Random r = new Random();
+            int i = (int)r.Next(1000);
+            int j = (int)r.Next(1000);
+            Display.Click().Send(i.ToString());
+            mult.Click();
+            Display.Click().Send(j.ToString());
+            equals.Click();
+            Assert.AreEqual(Display.Name, (i * j).ToString());
+        }
+    }
+}
---
+++ src/SampleTests/TestBaseNUnit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Drawing;
+using NUnit.Framework;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using WhiteSharp;
+
+namespace SampleTests
+{
+    [TestFixture]
+    public class TestBaseNUnit
+    {
+        Process proc = Process.Start(@"C:\Windows\System32\calc.exe");
+        public static UIWindow MainWindow;
+        public UIControl Display;
+
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [TestFixtureSetUp]
+        public void Iint()
+        {
+            Application app = Application.Attach(proc);
+            app.WaitWhileBusy();
+            MainWindow = new UIWindow("Calculator");
+            Display = MainWindow.FindControl(By.AutomationId("150"));
+            Assert.True(Display.Name.Equals("0"));
+        }
+        [TestFixtureTearDown]
+        public void Cleanup()
+        {
+            Process.GetProcessById(proc.Id).Kill();
+        }
+        [SetUp]
+        public void Start()
+        {
+            Logging.Start(TestContext.CurrentContext.Test.Name);
+        }
+        [TearDown]
+        public void Stop()
+        {
+            //Clear screen
+            MainWindow.FindControl(By.AutomationId("81")).Click();
+            Assert.True(Display.Name.Equals("0"));
+
+            //Take screenshot if test failed
+            if (TestContext.CurrentContext.Result.Status == TestStatus.Failed)
+            {
+                string name = TestContext.CurrentContext.Test.Name.ToString();
+                new TestStack.White.ScreenCapture().CaptureScreenShot().Save("Results\\" + name.Substring(0, name.IndexOf("(")) + ".bmp");
+            }
+            Logging.Write(TestContext.CurrentContext.Result.Status.ToString().ToUpper() + "!");
+        }
+    }
+}
