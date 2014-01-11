e8fe2c769a6ff8d26156c0bad307294a725e4a4a
ilya-murzinov <murz42@gmail.com>
2014-01-11 06:07:11 -0800

    Fixed test runner.

--- src/RunTests.bat
+++ src/RunTests.bat
@@ -1,6 +1,7 @@
 if not exist Results mkdir Results
+set name=WhiteSharp
 set nunit="%cd%\..\..\Tools\NUnit 2.6.3\bin"
 set report="%cd%\..\..\Tools\NUnit2Report.Console"
-%nunit%\nunit-console.exe WhiteSharp\bin\Debug\WhiteSharp.dll /out:Results\WhiteSharp.txt
-%report%\NUnit2Report.Console.exe --todir Results --out WhiteSharp.html --fileset=WhiteSharp\bin\Debug\TestResult.xml --lang ru
+%nunit%\nunit-console.exe WhiteSharp\bin\Debug\%name%.dll /out:Results\%name%.txt
+%report%\NUnit2Report.Console.exe --todir Results --out %name%.html --fileset=TestResult.xml --lang en
 pause
