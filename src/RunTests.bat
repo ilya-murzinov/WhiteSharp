94212ac2e450f9a2b6c233de5040eaed81dcd286
Ilya Murzinov <ilya-murzinov@users.noreply.github.com>
2014-01-13 22:18:10 +0300

    Update RunTests.bat

--- src/RunTests.bat
+++ src/RunTests.bat
@@ -4,4 +4,3 @@ set nunit="%cd%\..\..\Tools\NUnit 2.6.3\bin"
 set report="%cd%\..\..\Tools\NUnit2Report.Console"
 %nunit%\nunit-console.exe WhiteSharp\bin\Debug\%name%.dll /out:Results\%name%.txt
 %report%\NUnit2Report.Console.exe --todir Results --out %name%.html --fileset=TestResult.xml --lang en
-pause
