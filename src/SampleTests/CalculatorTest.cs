acfdae3502164b9b16f6d1fcd473b008cb57a53c
ilya-murzinov <murz42@gmail.com>
2014-01-19 09:16:34 -0800

    Rewrote sample texst using Screen Object pattern

--- src/SampleTests/CalculatorTest.cs
+++ src/SampleTests/CalculatorTest.cs
@@ -4,23 +4,120 @@ using WhiteSharp;
 
 namespace SampleTests
 {
+    public class CalculatorScreenObject
+    {
+        private UIWindow window;
+
+        private UIControl display;
+        private UIControl clearEverything;
+
+        private UIControl[] numbers = new UIControl[10];
+
+        private UIControl equals;
+        private UIControl add;
+        private UIControl substract;
+        private UIControl multiply;
+        private UIControl divide;
+
+        protected CalculatorScreenObject()
+        {
+            window = new UIWindow("Calculator");
+            display = window.FindControl("150");
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = window.FindControl((130 + i).ToString());
+            }
+            clearEverything = window.FindControl("82");
+            equals = window.FindControl("121");
+            add = window.FindControl("93");
+            substract = window.FindControl("94");
+            multiply = window.FindControl("92");
+            divide = window.FindControl("91");            
+        }
+
+        private static CalculatorScreenObject instance;
+        public static CalculatorScreenObject Instance
+        {
+            get { return instance ?? (instance = new CalculatorScreenObject()); }
+        }
+
+        public CalculatorScreenObject CheckDisplayTextEquals(string text)
+        {
+            AssertThat.AreEqual(display, text, display.GetText());
+            return this;
+        }
+        public CalculatorScreenObject ClearScreen()
+        {
+            clearEverything.Click();
+            return this;
+        }
+        public CalculatorScreenObject Press(int number)
+        {
+            foreach (char ch in number.ToString())
+            {
+                numbers[int.Parse(ch.ToString())].Click();
+            }
+            return this;
+        }
+        public CalculatorScreenObject PressEquals()
+        {
+            equals.Click();
+            return this;
+        }
+        public CalculatorScreenObject PressAdd()
+        {
+            add.Click();
+            return this;
+        }
+        public CalculatorScreenObject PressSubstract()
+        {
+            substract.Click();
+            return this;
+        }
+        public CalculatorScreenObject PressMultiply()
+        {
+            multiply.Click();
+            return this;
+        }
+        public CalculatorScreenObject PressDivide()
+        {
+            divide.Click();
+            return this;
+        }
+    }
+
+    public class AboutDialog
+    {
+        private UIWindow window;
+
+        private UIControl btnOk;
+
+        private static AboutDialog instance;
+        public static AboutDialog Instance
+        {
+            get { return instance ?? (instance = new AboutDialog()); }
+        }
+    }
+
     [TestFixture]
     public class CalculatorTest : TestBaseNUnit
     {
-        [TestCase("130", "0")]
-        [TestCase("131", "1")]
-        [TestCase("132", "2")]
-        [TestCase("133", "3")]
-        [TestCase("134", "4")]
-        [TestCase("135", "5")]
-        [TestCase("136", "6")]
-        [TestCase("137", "7")]
-        [TestCase("138", "8")]
-        [TestCase("139", "9")]
-        public void NumberButtonsTest(string buttonId, string result)
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        public void NumberButtonsTest(int number)
         {
-            UIControl button = MainWindow.FindControl(By.AutomationId(buttonId)).Click();
-            AssertThat.AreEqual(Display, Display.Name, result);
+            CalculatorScreenObject.Instance
+                .Press(number)
+                .CheckDisplayTextEquals(number.ToString())
+                .ClearScreen();            
         }
 
         [TestCase("1")]
@@ -35,18 +132,40 @@ namespace SampleTests
         [TestCase("0")]
         public void Add(string str)
         {
-            UIControl plus = MainWindow.FindControl(By.AutomationId("93"));
-            UIControl equals = MainWindow.FindControl(By.AutomationId("121"));
             var r = new Random();
             int i = r.Next(1000);
             int j = r.Next(1000);
-            Display.Click().Send(i.ToString());
-            plus.Click();
-            Display.Click().Send(j.ToString());
-            equals.Click();
-            AssertThat.AreEqual(Display, Display.Name, (i + j).ToString());
+            CalculatorScreenObject.Instance
+                .Press(i)
+                .PressAdd()
+                .Press(j)
+                .PressEquals()
+                .CheckDisplayTextEquals((i+j).ToString())
+                .ClearScreen();
         }
-
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("4")]
+        [TestCase("5")]
+        [TestCase("6")]
+        [TestCase("7")]
+        [TestCase("8")]
+        [TestCase("9")]
+        [TestCase("0")]
+        public void Substract(string str)
+        {
+            var r = new Random();
+            int i = r.Next(1000);
+            int j = r.Next(1000);
+            CalculatorScreenObject.Instance
+                .Press(i)
+                .PressSubstract()
+                .Press(j)
+                .PressEquals()
+                .CheckDisplayTextEquals((i - j).ToString())
+                .ClearScreen();
+        }
         [TestCase("1")]
         [TestCase("2")]
         [TestCase("3")]
@@ -59,16 +178,39 @@ namespace SampleTests
         [TestCase("0")]
         public void Multiply(string str)
         {
-            UIControl mult = MainWindow.FindControl(By.AutomationId("92"));
-            UIControl equals = MainWindow.FindControl(By.AutomationId("121"));
             var r = new Random();
             int i = r.Next(1000);
             int j = r.Next(1000);
-            Display.Click().Send(i.ToString());
-            mult.Click();
-            Display.Click().Send(j.ToString());
-            equals.Click();
-            AssertThat.AreEqual(Display, Display.Name, (i + j).ToString());
+            CalculatorScreenObject.Instance
+                .Press(i)
+                .PressMultiply()
+                .Press(j)
+                .PressEquals()
+                .CheckDisplayTextEquals((i * j).ToString())
+                .ClearScreen();
+        }
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("4")]
+        [TestCase("5")]
+        [TestCase("6")]
+        [TestCase("7")]
+        [TestCase("8")]
+        [TestCase("9")]
+        [TestCase("0")]
+        public void Divide(string str)
+        {
+            var r = new Random();
+            int i = r.Next(1000);
+            int j = r.Next(1000);
+            CalculatorScreenObject.Instance
+                .Press(i)
+                .PressDivide()
+                .Press(j)
+                .PressEquals()
+                .CheckDisplayTextEquals((i / j).ToString())
+                .ClearScreen();
         }
     }
 }
