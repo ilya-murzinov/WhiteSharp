700a3e2ca6a9a713fbea6edd70128ea923aee6ad
ilya-murzinov <murz42@gmail.com>
2014-04-26 04:03:11 -0700

    add sample test

---
+++ src/SampleTests/SampleTestScenario.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using SampleTests.ScreenObjects;
+using System.Windows.Automation;
+
+namespace SampleTests
+{
+    [TestClass]
+    [TestFixture]
+    public class SampleTestScenario
+    {
+        [TestFixtureSetUp]
+        public void Init()
+        {
+
+        }
+
+        [TestMethod]
+        [Test]
+        public void SampleTest()
+        {
+            MainWindowTab1.Instance
+                .SelectItemFromCombobox("Test4")
+                .SetItem1CheckboxState(ToggleState.On)
+                .SetItem2CheckboxState(ToggleState.On)
+                .SetItem1CheckboxState(ToggleState.Off)
+                .OpenSecondTab()
+                .SetTextToMultilineTextbox("some text")
+                .SelectRadiobuttonState();
+        }
+
+        [TestFixtureTearDown]
+        public void Shutdown()
+        {
+
+        }
+    }
+}
---
+++ src/SampleTests/ScreenObjects/MainWindowTab1.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Automation;
+using WhiteSharp;
+namespace SampleTests.ScreenObjects
+{
+    public class MainWindowTab1
+    {
+        private static MainWindowTab1 _instance;
+
+        private Window _window;
+        private Control _combobox;
+        private Control _item1Checkbox;
+        private Control _item2Checkbox;
+
+        private MainWindowTab1()
+        {
+            _window = new Window("MainWindow");
+            _combobox = _window.FindControl("AComboBox");
+            _item1Checkbox = _window.FindControl(By.ClassName("CheckBox").AndName("Item1"));
+            _item2Checkbox = _window.FindControl(By.ClassName("CheckBox").AndName("Item2"));
+        }
+        
+        public static MainWindowTab1 Instance
+        {
+            get { return _instance ?? (_instance = new MainWindowTab1()); }
+        }
+
+        public MainWindowTab1 SelectItemFromCombobox(string item)
+        {
+            _combobox.SelectItem(item);
+            return this;
+        }
+
+        public MainWindowTab1 SetItem1CheckboxState(ToggleState state)
+        {
+            _item1Checkbox.SetToggleState(state);
+            return this;
+        }
+
+        public MainWindowTab1 SetItem2CheckboxState(ToggleState state)
+        {
+            _item2Checkbox.SetToggleState(state);
+            return this;
+        }
+
+        public MainWindowTab2 OpenSecondTab()
+        {
+            _window.FindControl(By.Name("Input Controls").AndClassName("TabItem"))
+                .Click();
+            return MainWindowTab2.Instance;
+        }
+    }
+}
